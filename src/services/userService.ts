ea993b0adce5d738fca959c51c9aaec59cc81b9e
Alex Guo <aguo2@uiowa.edu>
2025-06-12 18:31:28 -0400

    LOGIN: added hashing to password

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,12 @@
 import prisma from '@/lib/prisma';
 import { userSchema } from "@/lib/validators/user";
 import type { Prisma } from '@/generated/prisma';
+import bcrypt from 'bcrypt';
+
 
 export async function createUser(data: Prisma.UserModelCreateInput) {
   const parsed = userSchema.parse(data); 
+  const hash = await bcrypt.hash(data.password, 10);
+  data.password = hash
   return await prisma.userModel.create({ data });
 }
