ea0a33a214e60fc8432b718eec025ec5b4270ce7
Alex Guo <aguo2@uiowa.edu>
2025-06-12 14:35:37 -0400

    PRISMA: add migration

---
+++ prisma/migrations/20250612183150_initialmigration/migration.sql
@@ -0,0 +1,103 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('APPLIED', 'INTERVIEWING', 'ACCEPTED', 'REJECTED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Experience" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "owninguserID" INTEGER NOT NULL,
+
+    CONSTRAINT "Experience_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Resume" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "s3link" TEXT NOT NULL,
+    "owninguserID" INTEGER NOT NULL,
+
+    CONSTRAINT "Resume_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "JobTag" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "JobTag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Application" (
+    "id" SERIAL NOT NULL,
+    "jobdescription" TEXT NOT NULL,
+    "applicationdate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "Status" NOT NULL,
+    "archivedon" TIMESTAMP(3),
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "hourlyrate" DECIMAL(10,2) NOT NULL,
+    "yearlysalary" DECIMAL(10,2) NOT NULL,
+    "resumeID" INTEGER,
+    "owninguserID" INTEGER NOT NULL,
+
+    CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ApplicationToJobTag" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_ApplicationToJobTag_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Experience_owninguserID_idx" ON "Experience"("owninguserID");
+
+-- CreateIndex
+CREATE INDEX "Resume_owninguserID_idx" ON "Resume"("owninguserID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "JobTag_name_key" ON "JobTag"("name");
+
+-- CreateIndex
+CREATE INDEX "Application_owninguserID_idx" ON "Application"("owninguserID");
+
+-- CreateIndex
+CREATE INDEX "_ApplicationToJobTag_B_index" ON "_ApplicationToJobTag"("B");
+
+-- AddForeignKey
+ALTER TABLE "Experience" ADD CONSTRAINT "Experience_owninguserID_fkey" FOREIGN KEY ("owninguserID") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Resume" ADD CONSTRAINT "Resume_owninguserID_fkey" FOREIGN KEY ("owninguserID") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Application" ADD CONSTRAINT "Application_resumeID_fkey" FOREIGN KEY ("resumeID") REFERENCES "Resume"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Application" ADD CONSTRAINT "Application_owninguserID_fkey" FOREIGN KEY ("owninguserID") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ApplicationToJobTag" ADD CONSTRAINT "_ApplicationToJobTag_A_fkey" FOREIGN KEY ("A") REFERENCES "Application"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ApplicationToJobTag" ADD CONSTRAINT "_ApplicationToJobTag_B_fkey" FOREIGN KEY ("B") REFERENCES "JobTag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
