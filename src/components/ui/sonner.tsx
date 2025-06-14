aff5596b383b62e5201327866f0d0e6ff8aecce9
Alex Guo <aguo2@uiowa.edu>
2025-06-14 14:25:54 -0400

    LAYOUT: sonnar colors are fixed

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -13,14 +13,14 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className="toaster group"
       style={
         {
-          "--normal-bg": `var(--${palette.neutralBg})`,
-          "--normal-text": `var(--${palette.neutralText})`,
-          "--normal-border": `var(--${palette.secondaryBorder})`,
-          "--info-bg": `var(--${palette.infoBg})`,
-          "--success-bg": `var(--${palette.successBg})`,
-          "--warning-bg": `var(--${palette.warningBg})`,
-          "--error-bg": `var(--${palette.dangerBg})`,
-          "--error-text": `var(--${palette.dangerText})`
+          "--normal-bg": "#292524", // neutralBg: bg-white
+          "--normal-text": "#FFFFFF", // neutralText: text-gray-800
+          "--normal-border": "#1c1917", // secondaryBorder: border-stone-800
+          "--info-bg": "#2dd4bf", // infoBg: bg-teal-400
+          "--success-bg": "#22c55e", // successBg: bg-green-500
+          "--warning-bg": "#fcd34d", // warningBg: bg-amber-300
+          "--error-bg": "#ef4444", // dangerBg: bg-red-500
+          "--error-text": "#ef4444" // dangerText: text-red-500
         } as React.CSSProperties
       }
       {...props}
