5845f2e36e7b0b34123a5c2929b21ad7288ab9da
Alex Guo <aguo2@uiowa.edu>
2025-06-15 13:25:37 -0400

    LOGOUT: added linkstyledbutton test

--- src/tests/components/link.spec.tsx
+++ src/tests/components/link.spec.tsx
@@ -1,16 +1,28 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import React from 'react'
-import {Link} from '@/components/link'
+import { Link, LinkStyledButton } from '@/components/link'
 
-describe('PasswordInput', () => {
-  beforeEach(() => {
-    render(<Link href="google.com">Text</Link>)
+describe('Link Components', () => {
+  describe('Link', () => {
+    beforeEach(() => {
+      render(<Link href="google.com">Text</Link>)
+    })
+
+    it('should render the correct text and link', () => {
+      const link = screen.getByText(/Text/i)
+      expect(link).toBeInTheDocument()
+      expect(link.tagName).toBe('A')
+      expect(link).toHaveAttribute('href', 'google.com')
+    })
   })
 
-  it('should render the correct text and link', () => {
-    const link = screen.getByText(/Text/i)
-    expect(link).toBeInTheDocument()
-    expect(link.tagName).toBe('A')
-    expect(link).toHaveAttribute('href', 'google.com')
+  describe('LinkStyledButton', () => {
+    it('should call handleClick on click', () => {
+      const handleClick = vi.fn()
+      render(<LinkStyledButton handleClick={handleClick}>Click</LinkStyledButton>)
+      const button = screen.getByText(/Click/i)
+      fireEvent.click(button)
+      expect(handleClick).toHaveBeenCalledTimes(1)
+    })
   })
 })
